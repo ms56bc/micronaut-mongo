648b76b24482b00470a88c3d5d7ec6feb85f082c
Naveen Dabas <ms56bc>
2023-01-15 11:55:23 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+## Micronaut 3.8.1 Documentation
+
+- [User Guide](https://docs.micronaut.io/3.8.1/guide/index.html)
+- [API Reference](https://docs.micronaut.io/3.8.1/api/index.html)
+- [Configuration Reference](https://docs.micronaut.io/3.8.1/guide/configurationreference.html)
+- [Micronaut Guides](https://guides.micronaut.io/index.html)
+---
+
+- [Jib Gradle Plugin](https://plugins.gradle.org/plugin/com.google.cloud.tools.jib)
+- [Shadow Gradle Plugin](https://plugins.gradle.org/plugin/com.github.johnrengelman.shadow)
+## Feature test-resources documentation
+
+- [Micronaut Test Resources documentation](https://micronaut-projects.github.io/micronaut-test-resources/latest/guide/)
+
+
+## Feature mongo-reactive documentation
+
+- [Micronaut MongoDB Reactive Driver documentation](https://micronaut-projects.github.io/micronaut-mongodb/latest/guide/index.html)
+
+- [https://docs.mongodb.com](https://docs.mongodb.com)
+
+
+## Feature jmx documentation
+
+- [Micronaut JMX endpoints documentation](https://micronaut-projects.github.io/micronaut-jmx/latest/guide/index.html)
+
+
+## Feature management documentation
+
+- [Micronaut Management documentation](https://docs.micronaut.io/latest/guide/index.html#management)
+
+
+## Feature http-client documentation
+
+- [Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#httpClient)
+
+
+## Feature kubernetes documentation
+
+- [Micronaut Kubernetes Support documentation](https://micronaut-projects.github.io/micronaut-kubernetes/latest/guide/index.html)
+
+- [https://kubernetes.io/docs/home/](https://kubernetes.io/docs/home/)
+
+
+## Feature data-mongodb-reactive documentation
+
+- [Micronaut Data MongoDB Reactive documentation](https://micronaut-projects.github.io/micronaut-data/latest/guide/#mongo)
+
+- [https://docs.mongodb.com](https://docs.mongodb.com)
+
+
---
+++ build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.6.21"
+    id("org.jetbrains.kotlin.kapt") version "1.6.21"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.6.21"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("io.micronaut.application") version "3.7.0"
+    id("com.google.cloud.tools.jib") version "2.8.0"
+    id("io.micronaut.test-resources") version "3.7.0"
+}
+
+version = "0.1"
+group = "com.learning"
+
+val kotlinVersion=project.properties.get("kotlinVersion")
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    kapt("io.micronaut:micronaut-http-validation")
+    kapt("io.micronaut.data:micronaut-data-document-processor")
+    kapt("io.micronaut.micrometer:micronaut-micrometer-annotation")
+    implementation("io.micronaut:micronaut-http-client")
+    implementation("io.micronaut:micronaut-jackson-databind")
+    implementation("io.micronaut:micronaut-management")
+    implementation("io.micronaut.data:micronaut-data-mongodb")
+    implementation("io.micronaut.jmx:micronaut-jmx")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.micrometer:micronaut-micrometer-core")
+    implementation("io.micronaut.micrometer:micronaut-micrometer-registry-datadog")
+    implementation("io.micronaut.micrometer:micronaut-micrometer-registry-jmx")
+    implementation("io.micronaut.mongodb:micronaut-mongo-reactive")
+    implementation("jakarta.annotation:jakarta.annotation-api")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("org.mongodb:mongodb-driver-reactivestreams")
+    implementation("io.micronaut:micronaut-validation")
+
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+}
+
+
+application {
+    mainClass.set("com.learning.ApplicationKt")
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("17")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+    jib {
+        to {
+            image = "gcr.io/myapp/jib-image"
+        }
+    }
+}
+graalvmNative.toolchainDetection.set(false)
+micronaut {
+    runtime("netty")
+    testRuntime("kotest")
+    processing {
+        incremental(true)
+        annotations("com.learning.*")
+    }
+}
+
+
+
